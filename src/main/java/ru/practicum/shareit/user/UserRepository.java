348baf2ec45780b84d0abef50a5e9b187c3e75a4
nazarov <9815444@gmail.com>
2022-10-27 14:47:30 +0300

    add DataJpaTest

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,9 +1,12 @@
 package ru.practicum.shareit.user;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.jupiter.api.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
