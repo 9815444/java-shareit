2f0daec77615584b3a734001bc06bed6b5c8c087
nazarov <9815444@gmail.com>
2022-10-27 14:52:49 +0300

    fixed

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.user;
 
-import com.google.common.annotations.VisibleForTesting;
-import org.junit.jupiter.api.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.model.User;
