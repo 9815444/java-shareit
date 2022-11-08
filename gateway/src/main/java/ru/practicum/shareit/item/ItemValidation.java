0513fb4775826d3107ecccd1deda8eef27a47871
nazarov <9815444@gmail.com>
2022-11-08 14:37:06 +0300

    corrections based on the results of the code review6

--- gateway/src/main/java/ru/practicum/shareit/item/ItemValidation.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemValidation.java
@@ -14,8 +14,6 @@ import java.util.Map;
 @AllArgsConstructor
 public class ItemValidation {
 
-//    private final UserValidation userValidation;
-
     private UserClient userClient;
     private ItemClient itemClient;
 
